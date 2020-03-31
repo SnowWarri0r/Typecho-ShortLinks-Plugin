bea8e50f617adaf519b35a3ee5159bba04b1a0e7
SnowWarri0r <62747881+SnowWarri0r@users.noreply.github.com>
2020-03-31 13:51:40 +0800

    Add files via upload

--- Plugin.php
+++ Plugin.php
@@ -10,10 +10,27 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
  */
 class ShortLinks_Plugin implements Typecho_Plugin_Interface
 {
+    /**
+     * 激活插件方法,如果激活失败,直接抛出异常
+     *
+     * @access public
+     * @return String
+     * @throws Typecho_Plugin_Exception
+     */
     public static function activate(){
-    }
 
-    public static function deactivate(){}
+    }
+    /**
+     * 禁用插件方法,如果禁用失败,直接抛出异常
+     *
+     * @static
+     * @access public
+     * @return String
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function deactivate(){
+
+    }
 
     public static function config(Typecho_Widget_Helper_Form $form){}
 
